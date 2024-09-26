7f492350760caf1fe418bb1605d900b3c2aa1c37
YanYang-G0001 <yanyang.gong@uzh.ch>
2024-09-26 13:02:38 +0200

    Added the 1st LLM prompt

--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,10 @@ from flask_restx import Resource, Api
 from flask_pymongo import PyMongo
 from pymongo.collection import Collection
 from .model import Company
+from src.llm.groq_llm import GroqClient
+import os
+import json
+
 # Configure Flask & Flask-PyMongo:
 app = Flask(__name__)
 # allow access from any frontend
@@ -52,7 +56,7 @@ class Companies(Resource):
             # create model
             model_ag = AutoReg(endog=profit_df['value'], lags=1, trend='c', seasonal=False, exog=None, hold_back=None,
                                period=None, missing='none')
-            # train the model
+            # train the modelllm/groq_api_poem.json
             fit_ag = model_ag.fit()
             # predict for 2022 based on the profit data
             prediction_value = fit_ag.predict(start=len(profit_df), end=len(profit_df), dynamic=False).values[0]
@@ -60,5 +64,21 @@ class Companies(Resource):
             company.profit.insert(0, {'year': 2022, 'value': prediction_value})
         return company.to_json()
 
+class PoemByCompanyId(Resource):
+    def get(self,companyId):
+        #create an instance
+        groq_client = GroqClient()
+        # Acquire company name by id firstly
+        company_data = companies.find_one_or_404({"id": companyId})
+        company_name = company_data["name"]
+        key_word = company_name.upper()
+        #generate poem
+        prompt_file_path='src/llm/groq_api_poem.json'
+        poem,response_code = groq_client.generate_poem(company_name, prompt_file_path)
+        return {"poem":poem,"name":key_word, "response_code":response_code}
+
+
+
 api.add_resource(CompaniesList, '/companies')
 api.add_resource(Companies, '/companies/<int:id>')
+api.add_resource(PoemByCompanyId, '/llm/groq/poem/<int:companyId>')
